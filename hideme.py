3377cdc3b9ac6eb0c627a574fcbbe07ac43a17d4
elf <61194758+e1fy@users.noreply.github.com>
2020-11-01 12:56:01 +0000

    Update hideme.py

--- hideme.py
+++ hideme.py
@@ -1,6 +1,6 @@
 # Real quick note. YOU MUST RUN THIS AS ROOT
 
-import requests,socket,struct, time, os
+import requests,socket,struct,os
 s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
 while True:
